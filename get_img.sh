3a6a5e5b4bec71130a62c38ef6894631b9bd61a8
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-12-09 17:00:28 +0530

    add numbers

--- get_img.sh
+++ get_img.sh
@@ -4,4 +4,11 @@ path="/home/thandai/code/rpi"
 
 ssh thandai@raspberrypi.local python ${path}/cam_save.py
 
+num_imgs=`ls -1 imgs/ | wc -l`
+new_num_imgs=`expr $num_imgs + 1`
+
 scp thandai@raspberrypi.local:${path}/cam_data.csv  ./imgs/
+
+mv ./imgs/cam_data.csv ./imgs/cam_data_${new_num_imgs}.csv
+
+
